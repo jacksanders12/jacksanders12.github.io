53bfb62781a6dc28d7f52104515f18adac32e6e0
jacks <jacks@iastate.edu>
2019-03-12 16:18:21 -0500

    changing to php and adding header

--- header.php
+++ header.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
   <title>Illini Magic</title>
   <meta charset="utf-8">
@@ -44,26 +42,3 @@
     </ul>
   </div>  
 </nav>
-
-<div class="container" style="margin-top:30px">
-  <div class="row">
-    <div class="col-sm-4">
-      <img src="img/illinimagic.jpg" width="100%">
-      
-      <hr class="d-sm-none">
-    </div>
-    <div class="col-sm-8">
-      <h2>Illini Magic</h2>
-      <h5>Springfield Illinois</h5>
-      <p>Illini Magic is a boys competitive basketball team based in Springfield Illinois since 2007</p>
-      <p><a href="./img/Payment_and_Tryout_Information.pdf">Tryout information </a> and <a href="./img/Tryout_Registration_Form.pdf">Registration Form </a></p>
-    </div>
-  </div>
-</div>
-
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <p>Twitter: @illini_magic</p>
-</div>
-
-</body>
-</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('header.php'); ?>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="img/illinimagic.jpg" width="100%">
+      
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>Illini Magic</h2>
+      <h5>Springfield Illinois</h5>
+      <p>Illini Magic is a boys competitive basketball team based in Springfield Illinois since 2007</p>
+      <p><a href="./img/Payment_and_Tryout_Information.pdf">Tryout information </a> and <a href="./img/Tryout_Registration_Form.pdf">Registration Form </a></p>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Twitter: @illini_magic</p>
+</div>
+
+</body>
+</html>
