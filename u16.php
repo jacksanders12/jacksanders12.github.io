44d3a09a6d156baf07287753279808ef4575c556
jacks <jacks@iastate.edu>
2019-03-12 16:58:54 -0500

    html test

--- u16.php
+++ u16.php
@@ -1,6 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include('header.php'); ?>
+<head>
+  <title>Illini Magic</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <style>
+  .img {
+      height: 200px;
+      background: #aaa;
+  }
+  </style>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="./index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="dropdown"><a class="navbar-brand" data-toggle="dropdown" href="#">Rosters </a>
+        <ul class="dropdown-menu">
+          <li class><a href="./u16.php">U16</a></li>
+          <li><a href="./u17.php">U17</a></li>
+          
+        </ul>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="./UpcomingEvents.php">Upcoming Events</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Contact</a>
+      </li>    
+      <li class="nav-item active">
+        <a class="nav-link" href="./AboutUs.php">About Us</a>
+      </li> 
+    </ul>
+  </div>  
+</nav>
+</head>
+
+<body>
+
 
 <div class="container">
   <h2>U16 Roster</h2>
