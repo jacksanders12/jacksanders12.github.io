98fddf9ca43713b4974dd6b6b0b4c64a43abc644
jacks <jacks@iastate.edu>
2019-03-13 16:24:13 -0500

    jquery test

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){ 
   $.get("header.html", function(data) {
     $("#header").html(data);
+    $alert("Hello! I am an alert box!!");
   });
 });
+
 
 
 function removeHidden() {
