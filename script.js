d3be7a1fa0fe5fda9ecb0b98ca4428f77c321386
jacks <jacks@iastate.edu>
2019-03-12 23:31:36 -0500

    jquery test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){ 
   $.get("header.html", function(data) {
     $("#header").html(data);
-    $("#mainContent").removeClass("hidden");
+    //$("#mainContent").removeClass("hidden");
   });
 });
 
