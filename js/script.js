f68bb5e8bc9040dbead811cec890a7fe0a491c3f
jacks <jacks@iastate.edu>
2019-03-13 18:25:23 -0500

    please stop blinking

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
 
 
 
-/*$( '#topheader .navbar-nav li' ).on( 'click', function () {
+$( '#topheader .navbar-nav li' ).on( 'click', function () {
 	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
-});*/
+});
 
